5d837865bb5e4a9974fadd8322589b355d2d2dcd
Siming Chen <susanchen1996@gmail.com>
2016-10-19 11:40:43 -0400

    fixed all polyline, json data unsolved

--- mbta/geolocation.js
+++ mbta/geolocation.js
@@ -1,10 +1,9 @@
-/* Part 1:
-	Set up the map in genreal
-	including init map, get my location, rendermap on web
-*/
+
 var myLat = 0;
 var myLng = 0;
 var me = new google.maps.LatLng(myLat, myLng);
+var request = new XMLHttpRequest();
+
 var mapOptions = {
 	center: me,
 	zoom: 15
@@ -13,51 +12,6 @@ var map;
 var marker_me;
 var infowindow = new google.maps.InfoWindow();
 
-//initiate google map 
-function init(){
-	map = new google.maps.Map(document.getElementById('map_onscreen'),mapOptions);
-	console.log(map);
-	getMyLocation();
-	set_Marker();
-}
-
-//get location for map
-function getMyLocation(){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(function(position){
-			myLat = position.coords.latitude;
-			myLng = position.coords.longitude;
-			renderMap();
-		});
-	}
-	else {
-		alert("Geolocation is not supported by your web browser.");
-	}
-}
-
-//center the map to current position and set up marker info window
-function renderMap(){
-	me = new google.maps.LatLng(myLat, myLng);
-
-	map.panTo(me);
-
-	marker_me = new google.maps.Marker({
-		position: me,
-		title: "here"
-	});
-
-	marker_me.setMap(map);
-
-	google.maps.event.addListener(marker_me, 'click', function(){
-		infowindow_me.setContent(nearest_station);
-		infowindow_me.open(map, marker_me);	
-	});
-
-}
-
-/* Part 2:
-	set marks for MBTA station and render polyline for redline
-*/
 var redline_station = [
 {lat: 42.395428, lng: -71.142483},
 {lat: 42.39674, lng: -71.121815},
@@ -106,19 +60,7 @@ var redline_name = [
 "Fields Corner",
 "Shawmut Station",
 "Ashmont Station"
-]
-
-//set marker for each station
-function set_Marker(){
-var image = "https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png";
-
-for (var i = 0; i < 22; i++){
-var marker_station = new google.maps.Marker({
-	position : redline_station[i],
-	icon: image,
-	map: map
-	});
-}
+];
 
 var redline_braintree = [
 {lat: 42.395428, lng: -71.142483},
@@ -139,7 +81,7 @@ var redline_braintree = [
 {lat:42.251809,lng:-71.005409},
 {lat:42.233391,lng:-71.007153},
 {lat:42.2078543, lng:-71.0011385}
-]
+];
 
 var redline_two = [
 {lat: 42.320685, lng:-71.052391},
@@ -147,7 +89,67 @@ var redline_two = [
 {lat:42.300093,lng:-71.061667},
 {lat:42.29312583, lng:-71.06573796000001},
 {lat:42.284652,lng:-71.06448899999999},
-]
+];
+
+/* Part 1:
+	Set up the map in genreal
+	including init map, get my location, rendermap on web
+*/
+//initiate google map 
+function init(){
+	map = new google.maps.Map(document.getElementById('map_onscreen'),mapOptions);
+	getMyLocation();
+	set_Marker(); //set marker only for all train statio 
+	request_info(); //request info from MBTA
+}
+
+//get location for map
+function getMyLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			renderMap();
+		});
+	}
+	else {
+		alert("Geolocation is not supported by your web browser.");
+	}
+}
+
+//center the map to current position and set up marker info window
+function renderMap(){
+	me = new google.maps.LatLng(myLat, myLng);
+
+	map.panTo(me);
+
+	marker_me = new google.maps.Marker({
+		position: me,
+		title: "here"
+	});
+
+	marker_me.setMap(map);
+
+	distance(); //find nearest and set info window
+	
+}
+
+
+/* Part 2:
+	set marks for MBTA station and render polyline for redline
+*/
+
+//set marker for each station
+function set_Marker(){
+var image = "https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png";
+
+for (var i = 0; i < 22; i++){
+var marker_station = new google.maps.Marker({
+	position : redline_station[i],
+	icon: image,
+	map: map
+	});
+}
 
 //render polyline on the map
 var redline_path = new google.maps.Polyline({
@@ -176,61 +178,71 @@ redline_pathtwo.setMap(map);
 	calculate distance of the closest station, render info in infowindow, 
 	render polyline from current position to nearest station
 */
-var R = 6371e3;
-var distance = 0;
 
-if (typeof(Number.prototype.toRad) == "undefined"){
-	Number.prototype.toRad = function(){
-		return this * Math.PI / 180;
+function distance(){
+	var R = 6371e3;
+	var distance = 0;
+
+	if (typeof(Number.prototype.toRad) == "undefined"){
+		Number.prototype.toRad = function(){
+			return this * Math.PI / 180;
+		}
 	}
-}
-
 //calculate distance
-var counter = 0;
-for (i = 0; i < 22; i++){
-	var lat1 = redline_station[i].lat.toRad();
-	var lat2 = myLat.toRad();
-	var LAT = (lat2 - lat1).toRad();
-	var LNG = (myLng - redline_station[1].lng).toRad();
+	var counter = 0;
+	for (i = 0; i < 22; i++){
 
-	var a = Math.sin(LAT/2) * Math.sin(LAT/2) + Math.cos(lat1) * Math.cos(lat2) *
-	Math.sin(LNG/2) * Math.sin(LNG/2);
+		var lat1 = redline_station[i].lat.toRad();
+		var lat2 = myLat.toRad();
+		var LAT = (lat2 - lat1).toRad();
+		var LNG = (myLng - redline_station[i].lng).toRad();
 
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		var a = Math.sin(LAT/2) * Math.sin(LAT/2) + 
+			Math.cos(lat1) * Math.cos(lat2) *
+			Math.sin(LNG/2) * Math.sin(LNG/2);
 
-	var d = R * c;
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-	if (i == 0){
-		distance = d;
-	}else if (d < distance){
-		distance = d;
-		counter = i;
+		var d = R * c;
+
+		//console.log(d);
+		if (i == 0){
+			distance = d;
+		}else if (d < distance){
+			distance = d;
+			counter = i;
+		}
 	}
-}
+	//console.log(distance);
 
 //set info to my location marker
-var nearest_station = 'Nearest RedLine Station: ' + redline_name[counter] +"<br>" + 
-'Distance: ' + distance + ' miles';
+	var nearest_station = 'Nearest RedLine Station: ' + redline_name[counter] +"<br>" + 
+	'Distance: ' + distance + ' miles';
 
-var infowindow_me = new google.maps.InfoWindow({
-	content : nearest_station
-});
+	var infowindow_me = new google.maps.InfoWindow({
+		content : nearest_station
+	});
+
+	google.maps.event.addListener(marker_me, 'click', function(){
+		infowindow_me.setContent(nearest_station);
+		infowindow_me.open(map, marker_me);	
+	});
 
 //render a polyline from me to nearest station
-// HAVE a Problem rendering the line here!!!
-function blueline(){
-var nearest_distance = [
-	{lat: myLat, lng: myLng},
-	redline_station[counter]
-];
+	var nearest_distance = [
+		{lat: myLat, lng: myLng},
+		redline_station[counter]
+	];
 
-var blueline = new google.maps.Polyline({
-	path : nearest_distance,
-	geodesic: true,
-	strokeColor: '#FF0000',
-	strokeOpacity: 1.0,
-	strokeWeight: 3
-});
+	var blueline = new google.maps.Polyline({
+		path : nearest_distance,
+		geodesic: true,
+		strokeColor: '#00008B',
+		strokeOpacity: 1.0,
+		strokeWeight: 3
+	});
+	
+	blueline.setMap(map);
 }
 
 
@@ -238,21 +250,25 @@ var blueline = new google.maps.Polyline({
 	set train info for each station marker
 
 */
-var request = new XMLHttpRequest();
 
-request.open("get", "https://rocky-taiga-26352.herokuapp.com/redline.json", true);
-request.onreadystatechange = metroline;
-request.send();
+function request_info(){
 
-function metroline(){
-	console.log("The data is =" + request.responseText);
+	request.open("get", "https://rocky-taiga-26352.herokuapp.com/redline.json", true);
+	request.onreadystatechange = metroinfo;
+	request.send();
 
+}
+
+function metroinfo(){
 	if (request.readyState == 4 && request.status == 200){
 
-		message = request.responseText;
-		metroData = JSON.parse (message);
+		//message = request.responseText;
+		metroData = JSON.parse (request.responseText);
 
-	}
+		console.log(metroData);
+
+	}else{}
+
 }
 
 
